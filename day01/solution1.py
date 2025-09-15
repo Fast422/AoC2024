2462072104b9e44d3933420030dc62c78cdb6222
Fast84 <97892589+Fast422@users.noreply.github.com>
2025-09-15 17:39:41 +0100

    Add day01 solutions and input with VSCode debug config
    Added input1.txt with data for day01, initial solution1.py script to process the input, and an empty solution2.py. Also included a VSCode launch.json for debugging

---
+++ day01/solution1.py
@@ -0,0 +1,21 @@
+leftNums = []
+rightNums = []
+
+with open("input1.txt", "r+") as f:
+    for line in f.readlines():
+        left, right = line.strip().split("   ")
+
+        leftNums.append(int(left))
+        rightNums.append(int(right))
+
+leftNums.sort()
+rightNums.sort()
+# print(leftNums[:10])
+# print(rightNums[:10])
+
+total = 0
+
+for i in range(len(leftNums)):
+    total += abs(leftNums[i] - rightNums[i])
+
+print(total)
