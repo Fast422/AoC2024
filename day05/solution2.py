9687cb20d233d8124d0d8e58b91627e4659443bd
Fast84 <97892589+Fast422@users.noreply.github.com>
2025-09-17 15:38:03 +0100

    Update solution2.py

--- day05/solution2.py
+++ day05/solution2.py
@@ -10,19 +10,51 @@ pages = [list(map(int, line.split(","))) for line in lines[separator_idx+1:]]
 # print(instructions)
 # print(pages)
 tracker = defaultdict(list)
+global_graph = defaultdict(list)
 
 for instruction in instructions:
     before, after = map(int, instruction.split("|"))
     tracker[after].append(before)
+    global_graph[before].append(after)
 
-print(tracker)
+def reorder_pages(pages):
+    #Re-ordering
+    graph = defaultdict(list)
+    numbers = set(pages)
+    #print(f"Set of numbers: {numbers}")
+    for number in numbers:
+        all_dependencies = global_graph[number]
+        #print(f"All dependencies for {number}: {all_dependencies}")
+        required_dependencies = [x for x in all_dependencies if x in numbers]
+        print(f"Required dependencies for {number}: {required_dependencies}")
+        graph[number] = required_dependencies
+        #Pull out only the ones that are needed in the graph
+    print(graph)
 
-res = []
+    visited = set()
+    stack = []
+
+    def dfs(u):
+        visited.add(u)
+        for v in graph[u]:
+            if v not in visited:
+                dfs(v)
+        stack.append(u)
+
+    for u in pages:
+        if u not in visited:
+            dfs(u)
+    order = stack[::-1]
+    print(order)
+    mid = order[len(order) // 2]
+    return mid
+
+res = 0
 for page in pages:
     seen = set()  
     pages_set = set(page)   
     valid = True
-    print(pages_set)
+    #print(pages_set)
     for num in page:
         requirements = tracker[num]
         for requirement in requirements:
@@ -32,7 +64,7 @@ for page in pages:
         seen.add(num)
         if not valid:
             break
-
     if not valid:
-        
-print(sum(res))
+        res += reorder_pages(page)
+
+print(res)
