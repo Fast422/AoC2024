de5e61e03a19f3df0d8315cae1be421d930036ed
Fast84 <97892589+Fast422@users.noreply.github.com>
2025-09-27 14:43:22 +0100

    Add day07 input and solutions
    Added input.txt and two solution scripts (solution1.py, solution2.py) for day07. These files likely correspond to a coding challenge or puzzle, providing both the problem data and two approaches to solving it.

---
+++ day07/solution2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def parse(self):
+        with open("input.txt", "r") as f:
+            self.lines = [line.replace(":", "").strip().split(" ") for line in f.readlines()]
+    
+    def solve(self):
+        values = []
+        for equation in self.lines:
+            equation = list(map(int, equation))
+            target = equation[0]
+            equation = equation[1:]
+            #print(equation, target)
+            def dfs(i, c):
+                #print(i,c)
+                if i == len(equation):
+                    return c == target
+
+                return dfs(i+1, c+equation[i]) or dfs(i+1, c*equation[i]) or dfs(i+1, int(str(c) + str(equation[i])))
+            if dfs(1, equation[0]):
+                values.append(target)
+        print(sum(values))
+
+if __name__ == "__main__":
+    sol = Solution()
+    sol.parse()
+    sol.solve()
